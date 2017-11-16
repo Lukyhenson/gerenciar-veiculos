9988a4f3c68cfdd4f49220aecec123ec930eb011
Tauan <lukyhenson@gmail.com>
2017-11-16 12:04:15 -0200

    attr

---
+++ Back-End/gerenciarVeiculos/src/main/java/com/projetoftt/gerenciarVeiculos/GerenciarVeiculosBackendApplication.java
@@ -0,0 +1,12 @@
+package com.projetoftt.gerenciarVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarVeiculosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarVeiculosBackendApplication.class, args);
+	}
+}
---
+++ Back-End/gerenciarVeiculos/src/test/java/com/projetoftt/gerenciarVeiculos/GerenciarVeiculosBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetoftt.gerenciarVeiculos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciarVeiculosBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
