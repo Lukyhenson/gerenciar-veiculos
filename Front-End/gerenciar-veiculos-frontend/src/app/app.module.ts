2ed1a383bebdbcc710f28520bf5756b00c0d6e07
lukyhenson <lukyhenson@live.com>
2017-11-17 03:49:08 -0200

    add navbar and sidebar

--- Front-End/gerenciar-veiculos-frontend/src/app/app.module.ts
+++ Front-End/gerenciar-veiculos-frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
