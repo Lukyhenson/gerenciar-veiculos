5152969eb5645d37f87c3040e03c52a2a76854ab
lukyhenson <lukyhenson@live.com>
2017-11-20 11:55:40 -0200

    adicionado rota home

--- Front-End/gerenciar-veiculos-frontend/src/app/app.module.ts
+++ Front-End/gerenciar-veiculos-frontend/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideRoutes} from '@angular/router';
+import { Http, HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { HomeComponent } from './home/home.component';
@@ -27,7 +29,9 @@ import { FabricanteComponent } from './admin/fabricante/fabricante.component';
   ],
   imports: [
     HomeModule,
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
